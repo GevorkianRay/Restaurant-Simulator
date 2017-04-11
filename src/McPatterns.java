98564b16754694693afc1eafcf71c74cf339d30b
Raymond Gevorkian <gevorkianray@gmail.com>
2017-04-10 17:16:04 -0700

    Update McPatterns.java

--- src/McPatterns.java
+++ src/McPatterns.java
@@ -1,8 +1,5 @@
 /**
  * @Raymond Gevorkian April 7th - April 14th 2016
- * Attributions to: CS Club Tutoring, Professor Rangasayee Office Hours / source code, online sources, and some purely logical advice from peers. 
- * Note: all logical advice from peers has been noted within the code. 
- * Note: all online sources have been noted within the code or README file.
  * This program loads a menu file to stimulate a touch screen ordering system.
  * The menu is displayed and adds each ordered item as buttons on the screen.
  * The total price is always present and displayed accurately.
